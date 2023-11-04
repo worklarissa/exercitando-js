d6204e4d653237faaccd57e83af92331d0517d93
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2023-11-04 13:47:40 -0300

    comecei a revisao

--- logica1/revisao.js
+++ logica1/revisao.js
@@ -1,1 +1,22 @@
 //aula de revisão de lógicaI
+
+/*programa que salva dois numeros e realiza soma, subtração,
+ multiplicação e divisão*/
+/*let numeroUm = 2;
+let numeroDois = 5;
+
+let soma = numeroUm + numeroDois;
+let subtracao = numeroDois - numeroUm;
+let divisao = numeroDois / numeroUm;
+let multiplicacao = numeroUm * numeroDois;
+
+console.log(soma, subtracao, divisao, multiplicacao)*/
+
+//programa que armazena dois números e retorna se o primeiro é maior que o segundo
+
+let numUm = 13
+let numDois = 6
+
+let maiorQue = numeroUm > numeroDois
+
+console.log(maiorQue)
