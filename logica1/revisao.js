7396a945829f20c3b07e6e8e765e890508775285
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2023-11-04 16:24:36 -0300

    terminei logica1

--- logica1/revisao.js
+++ logica1/revisao.js
@@ -12,11 +12,34 @@ let multiplicacao = numeroUm * numeroDois;
 
 console.log(soma, subtracao, divisao, multiplicacao)*/
 
-//programa que armazena dois números e retorna se o primeiro é maior que o segundo
+//programa que armazena dois números e retorna se o primeiro 
+//é maior que o segundo
 
-let numUm = 13
+/*let numUm = 13
 let numDois = 6
 
-let maiorQue = numeroUm > numeroDois
+let maiorQue = numUm > numDois
 
-console.log(maiorQue)
+console.log(maiorQue)*/
+
+//programa de login
+/*let nomeUsuario = "admin" 
+let senha = "12345"
+
+let verificacao = (nomeUsuario == "admin" && senha == "12345") 
+
+
+console.log(`login correrto: ${verificacao}`)*/
+
+/*programa que calcula a média entre 4 notas. As notas podem variar de
+ 0.0 a 10.0. Deve calcular e exibir o resultado*/
+
+ let notaUm = 5.44
+ let notaDois = 6.12
+ let notaTres = 3.5
+ let notaQuatro = 8.7
+
+ let somaDasQuatro = notaUm + notaDois + notaTres + notaQuatro
+ let mediaFinal = (somaDasQuatro / 4).toFixed(2)
+
+ console.log(`Sua nota é ${mediaFinal}`)
