4d4f0187c809da839d6298d4f03e70eaced4fb8d
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2023-12-21 21:08:04 -0300

    terminei a aula 2 dee lógica II

--- logica2/aula2.js
+++ logica2/aula2.js
@@ -1,8 +1,8 @@
-//exercício junto com a professora
+//exercício junto com a professora- switch case
 /*Faça um programa que recebe um código de status de pedido(ex:"PENDENTE", "EM_ANDAAMENTO", "ENTREGUE") como entrada.
 Utilize switch case para verificar o código de status e imprimir a mensagem correspondente (ex: "Seu pedido está pendente de pagamento")
 Caso o pedido não esteja em nenhum dos status acima, imprima a mensagem "status invélido ou pedido não computado".*/
-let statusDePedido = "PENDENTE";
+/*let statusDePedido = "PENDENTE";
 
 switch(statusDePedido) {
     case "PENDENTE":
@@ -17,5 +17,24 @@ switch(statusDePedido) {
     default:
         console.log(`Status inválido ou pedido não computado`)
     
-}
-//exerc
+}*/
+
+//exercício array
+
+let cantoras = ["Beyonce", "Iza", "Rihanna", "Anitta", "Liniker" ]
+
+cantoras.push("Ludmilla")
+console.log (cantoras)
+
+cantoras.pop()
+console.log(cantoras)
+
+cantoras.unshift("Talia")
+console.log(cantoras)
+
+cantoras.shift()
+console.log(cantoras)
+
+//console.log(cantoras[4])
+console.log(cantoras[cantoras.length - 1])
+
