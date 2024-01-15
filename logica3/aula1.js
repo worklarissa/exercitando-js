abca3648a534221088b775975ff308c2a55f63b3
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-01-15 16:17:31 -0300

    Continuação de lógica 3 aula 1

--- logica3/aula1.js
+++ logica3/aula1.js
@@ -1,5 +1,20 @@
-function meuNome() {
+/*function meuNome() {
     console.log("Larissa");
 }
 
-meuNome()
+meuNome()*/
+
+//exercício um
+/*function nomeIdade(nome, idade) {
+    console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`)
+}
+
+nomeIdade("Larissa", 19)*/
+
+function mediaDeNotas(nota1, nota2, nota3) {
+    let soma = nota1 + nota2 + nota3
+    let calculo = soma / 3
+    console.log(`Sua nota é:  ${calculo}`) 
+}
+
+mediaDeNotas(10, 5, 8);
