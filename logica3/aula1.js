6ba2fba7eb320b07638ede2fe43e1ffa903e6a4a
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-12 20:59:06 -0300

    terminei aula1 de logica3

--- logica3/aula1.js
+++ logica3/aula1.js
@@ -11,6 +11,7 @@ meuNome()*/
 
 nomeIdade("Larissa", 19)*/
 
+/*exercício dois
 function mediaDeNotas(nota1, nota2, nota3) {
     let soma = nota1 + nota2 + nota3
     let calculo = soma / 3
@@ -18,3 +19,29 @@ function mediaDeNotas(nota1, nota2, nota3) {
 }
 
 mediaDeNotas(10, 5, 8);
+*/
+
+//exercio três
+ /*let segundos = 1800000
+function tempo (segundos){
+    minutos = segundos / 60
+    console.log (`Esse tempo em segundos é igual a ${minutos} minutos`)
+    horas = segundos / 3600
+    console.log (`Esse tempo em segundos é igual a ${horas} horas`)
+}
+
+tempo (segundos)*/
+
+const verificaMaiorNumero = (n1, n2, n3) => {
+    if (n1 > n2 && n1 > n3) {
+        console.log (`O maior número é o ${n1}`);
+    } 
+    else if (n2 > n1 && n2 > n3) {
+        console.log (`O maior número é o ${n2}`);
+    } 
+    else if (n3 > n1 && n3 > n2) {
+        console.log (`O maior número é o ${n3}`);
+    }
+}
+
+verificaMaiorNumero (4, 21, 12)
