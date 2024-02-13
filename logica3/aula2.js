d29f3efd65c24e05a037c88c0220bc6b525ad51c
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-13 17:17:59 -0300

    metade aula2 logica3

--- logica3/aula2.js
+++ logica3/aula2.js
@@ -1,10 +1,18 @@
 //exercicio um
-const mostrarTabuada = (numero) => {
+/*const mostrarTabuada = (numero) => {
     for (let i = 1 ; i <= 10; i++)  {
         console.log(numero * i);
     }
 }
 
 mostrarTabuada(9);
+*/
 
-//
+//exercicio dois- forEach
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+
+numeros.forEach((numero) => {
+    console.log(numero);
+})
+
