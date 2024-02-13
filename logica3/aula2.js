aad434791dc48d94454e2f5bdff4de5808d1ff00
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-13 16:46:46 -0300

    inicio da aula 2 logica3

--- logica3/aula2.js
+++ logica3/aula2.js
@@ -1,8 +1,10 @@
-multiplicador = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-function tabuada (numero, multiplicador) {
-    for (numero >= 0 ); {
-        conta = multiplicador[0] * multiplicador[i];
-        console.log(conta);
+//exercicio um
+const mostrarTabuada = (numero) => {
+    for (let i = 1 ; i <= 10; i++)  {
+        console.log(numero * i);
     }
 }
+
+mostrarTabuada(9);
+
+//
