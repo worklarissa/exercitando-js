d2a8c99d1a672fe179294b48054a0bd55690a737
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-02-12 21:20:50 -0300

    comecei aula 2 de logica3

---
+++ logica3/aula2.js
@@ -0,0 +1,8 @@
+multiplicador = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function tabuada (numero, multiplicador) {
+    for (numero >= 0 ); {
+        conta = multiplicador[0] * multiplicador[i];
+        console.log(conta);
+    }
+}
